fafac21ac5186d998d0b84de3e58b0eee1b52267
manaomimasha <123413831+manaomimasha@users.noreply.github.com>
2023-07-03 17:17:32 -0300

    ffdgdf

--- js/lib/stats.js
+++ js/lib/stats.js
@@ -38,15 +38,15 @@ var Stats = function () {
 
 	}
 
-	var msDiv = document.createElement( 'div' );
-	msDiv.id = 'ms';
-	msDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#020;display:none';
-	container.appendChild( msDiv );
+	// var msDiv = document.createElement( 'div' );
+	// msDiv.id = 'ms';
+	// msDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#020;display:none';
+	// container.appendChild( msDiv );
 
-	var msText = document.createElement( 'div' );
-	msText.id = 'msText';
-	msText.style.cssText = 'color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
-	msText.innerHTML = 'MS';
+	// var msText = document.createElement( 'div' );
+	// msText.id = 'msText';
+	// msText.style.cssText = 'color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
+	// msText.innerHTML = 'MS';
 	// msDiv.appendChild( msText );
 
 	// var msGraph = document.createElement( 'div' );
@@ -109,7 +109,7 @@ var Stats = function () {
 			msMin = Math.min( msMin, ms );
 			msMax = Math.max( msMax, ms );
 
-			msText.textContent = ms + ' MS (' + msMin + '-' + msMax + ')';
+			// msText.textContent = ms + ' MS (' + msMin + '-' + msMax + ')';
 			updateGraph( msGraph, Math.min( 30, 30 - ( ms / 200 ) * 30 ) );
 
 			frames ++;
